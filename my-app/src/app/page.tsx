b65ef2c4066e6d5f0c16a86fd2b5cda2bd36aa8e
viniciusvieirac <vini.clau0@gmail.com>
2023-08-03 12:05:51 -0300

    server side props

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,41 @@
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;  
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({recommendedProducts}: HomeProps) {
+  console.log(recommendedProducts);
   return (
     <div>
       <h1>Hello World</h1>
+      <section>
+        <h1>Products</h1>
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   )
 };
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
+}
